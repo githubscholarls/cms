30b791bc9cd7f0ac853385dcd8298e5fa40064ce
lishuai_schoalr <2363502880@qq.com>
2024-05-17 10:47:49 +0800

    调试linux下画图

---
+++ src/SSCMS.Core/StlParser/StlElementWT/StlWTFL.cs
@@ -0,0 +1,199 @@
+﻿using Datory.Utils;
+using Microsoft.AspNetCore.Components;
+using NPOI.SS.Formula;
+using NSwag;
+using Org.BouncyCastle.Asn1.X509;
+using SSCMS.Core.StlParser.Attributes;
+using SSCMS.Parse;
+using SSCMS.Services;
+using SSCMS.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using static System.Net.WebRequestMethods;
+
+namespace SSCMS.Core.StlParser.StlElementWT
+{
+    [StlElement(Title = "获取物通网201友情链接")]
+    public static class StlWTFL
+    {
+        public const string ElementName = "stl:wtfl";
+
+        private const string FromPro = nameof(FromPro);
+        private const string FromCity = nameof(FromCity);
+        private const string FromArea = nameof(FromArea);
+        private const string ToPro = nameof(ToPro);
+        private const string ToCity = nameof(ToCity);
+        private const string ToArea = nameof(ToArea);
+
+        public static async Task<object> ParseAsync(IParseManager parseManager)
+        {
+            var attributes = new NameValueCollection();
+
+            var fromPro = string.Empty;
+            var fromCity = string.Empty;
+            var fromArea = string.Empty;
+            var toPro = string.Empty;
+            var toCity = string.Empty;
+            var toArea = string.Empty;
+
+            foreach (var name in parseManager.ContextInfo.Attributes.AllKeys)
+            {
+                var value = parseManager.ContextInfo.Attributes[name];
+                if (StringUtils.EqualsIgnoreCase(name, FromPro))
+                {
+                    fromPro = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, FromCity))
+                {
+                    fromCity = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, FromArea))
+                {
+                    fromArea = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, ToPro))
+                {
+                    toPro = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, ToCity))
+                {
+                    toCity = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, ToArea))
+                {
+                    toArea = value;
+                }
+                else
+                {
+                    attributes[name] = value;
+                }
+            }
+            return await ParseAsync(parseManager, fromPro, fromCity, fromArea, toPro, toCity, toArea, attributes);
+        }
+
+        private static async Task<string> ParseAsync(IParseManager parseManager, string fromPro, string fromCity, string fromArea, string toPro, string toCity, string toArea, NameValueCollection attributes)
+        {
+
+            string href = string.Empty;
+            try
+            {
+#if DEBUG
+                Environment.SetEnvironmentVariable("wtapi", "https://webapi.chinawutong.com");
+#endif
+
+                href = await Get201Url(Environment.GetEnvironmentVariable("wtapi") ?? "http://192.168.0.154", fromPro, fromCity, fromArea, toPro, toCity, toArea);
+            }
+            catch (Exception)
+            {
+                href = await Get201Url("https://webapi.chinawutong.com", fromPro, fromCity, fromArea, toPro, toCity, toArea);
+            }
+
+
+            var from = GetShortAddr(fromPro, fromCity, fromArea);
+            var to = GetShortAddr(toPro, toCity, toArea);
+
+            return $"<a target=\"_black\" href='https://www.chinawutong.com/201{href}' {TranslateUtils.ToAttributesString(attributes)}>{from}到{to}物流公司</a>";
+
+        }
+
+        private static async Task<string> Get201Url(string wtapi, string fromPro, string fromCity, string fromArea, string toPro, string toCity, string toArea)
+        {
+            var url = $"{wtapi}/wlpageview/rediecturl/urlturned/?listwlline=" + HttpUtility.UrlEncode($"f={fromPro}-{fromCity}-{fromArea}&t={toPro}-{toCity}-{toArea}");
+
+            var resString = await HttpClientUtils.GetStringAsync(url);
+            if (string.IsNullOrEmpty(resString))
+            {
+                return string.Empty;
+            }
+            var res = Newtonsoft.Json.JsonConvert.DeserializeObject<UrlTurnedRes>(resString);
+            if (res is null || res.success != "true")
+            {
+                return string.Empty;
+            }
+            return res.data;
+        }
+        /// <summary>
+        /// {
+        ///    "success": "true",
+        ///    "errcode": "",
+        ///    "msg": "",
+        ///    "data": "/s5923227/"
+        ///}
+        /// </summary>
+        private class UrlTurnedRes
+        {
+            public string success { get; set; }
+            public string errcode { get; set; }
+            public string msg { get; set; }
+            public string data { get; set; }
+        }
+
+
+        public static string GetShortAddr(string pro, string city, string area)
+        {
+            try
+            {
+
+                string param = GetShortAddr(area);
+                if (string.IsNullOrWhiteSpace(param))
+                {
+                    param = GetShortAddr(city);
+                    if (string.IsNullOrWhiteSpace(param))
+                    {
+                        param = GetShortAddr(pro);
+                    }
+                }
+                return param;
+            }
+            catch
+            {
+            }
+
+            return "";
+        }
+
+        public static string GetShortAddr(string param, bool ispro = false)
+        {
+
+            try
+            {
+                /*
+             ①通用：2个汉字不处理
+             ②去掉≥3字以上：“地区”以及最后1字“市”“州”
+             ③去掉≥4字以上的：“盟”“自治州”“自治县”“自治旗”“直辖县”“市辖区”
+             */
+                if (!string.IsNullOrWhiteSpace(param) && param != "市辖区")
+                {
+                    if (param.Length > 4)
+                    {
+                        param = param.Replace("盟", "").Replace("自治州", "").Replace("自治县", "").Replace("自治旗", "").Replace("直辖县", "").Replace("市辖区", "");
+                    }
+                    if (param.Length >= 3)
+                    {
+                        if (param.Substring(param.Length - 2, 2) == "地区")
+                            param = param.Substring(0, param.Length - 2);
+                        else if (param.Substring(param.Length - 1, 1) == "市")
+                            param = param.Substring(0, param.Length - 1);
+                        else if (param.Substring(param.Length - 1, 1) == "州")
+                            param = param.Substring(0, param.Length - 1);
+                    }
+                    if (ispro)
+                    {
+                        param = param.Replace("省", string.Empty).Replace("区", string.Empty).Replace("自治区", "").Replace("直辖市", "").Replace("特别行政区", "");
+                    }
+                    return param;
+                }
+            }
+            catch
+            {
+            }
+            return "";
+        }
+    }
+}
+
---
+++ src/SSCMS.Core/StlParser/StlElementWT/StlWTImgLBFZ.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using SSCMS.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.Fonts;
+using Senparc.Weixin.Annotations;
+using System.Security.Policy;
+using System.Security.Cryptography;
+using Senparc.NeuChar.App.AppStore;
+using MathNet.Numerics;
+using DocumentFormat.OpenXml.Bibliography;
+using SSCMS.Utils;
+
+namespace SSCMS.Core.StlParser.StlElement
+{
+    /// <summary>
+    /// 礼邦分站画图
+    /// </summary>
+    public static class StlWTImgLBFZ
+    {
+        public const string ElementName = "stl:wtlbfz";
+        private const string SiteName = nameof(SiteName);
+        private const string Id = nameof(Id);
+        private const string Title = nameof(Title);
+        private const string LogoUp = nameof(LogoUp);
+        private const string LogoDown = nameof(LogoDown);
+        private const string From = nameof(From);
+        private const string To = nameof(To);
+        private const string Description = nameof(Description);
+        private const string DeliveryArea = nameof(DeliveryArea);
+
+
+        public static async Task<object> ParseAsync(IParseManager parseManager)
+        {
+
+
+            var webroot = parseManager.SettingsManager.WebRootPath;
+            var WTData = Path.Combine(webroot, "WTData");
+            if (!Directory.Exists(WTData))
+            {
+                Directory.CreateDirectory(WTData);
+            }
+            var ImgData = Path.Combine(WTData, "Image");
+            if (!Directory.Exists(ImgData))
+            {
+                Directory.CreateDirectory(ImgData);
+            }
+
+
+            var id = string.Empty;
+            var title = string.Empty;
+            var logoUp = string.Empty;
+            var logoDown = string.Empty;
+            var from = string.Empty;
+            var to = string.Empty;
+            var description = string.Empty;
+            var deliveryArea = string.Empty;
+
+
+
+            foreach (var name in parseManager.ContextInfo.Attributes.AllKeys)
+            {
+                var value = parseManager.ContextInfo.Attributes[name];
+                if (StringUtils.EqualsIgnoreCase(name, Id) && !value.StartsWith("{"))
+                {
+                    id = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, Id))
+                {
+                    id = await parseManager.ReplaceStlEntitiesForAttributeValueAsync(value);
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, Title) && !value.StartsWith("{"))
+                {
+                    title = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, Title))
+                {
+                    title = await parseManager.ReplaceStlEntitiesForAttributeValueAsync(value);
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, LogoUp)&&!value.StartsWith("{"))
+                {
+                    logoUp = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, LogoUp))
+                {
+                    logoUp = await parseManager.ReplaceStlEntitiesForAttributeValueAsync(value);
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, LogoDown) && !value.StartsWith("{"))
+                {
+                    logoDown = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, LogoDown))
+                {
+                    logoDown = await parseManager.ReplaceStlEntitiesForAttributeValueAsync(value);
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, From) && !value.StartsWith("{"))
+                {
+                    from = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, From))
+                {
+                    from = await parseManager.ReplaceStlEntitiesForAttributeValueAsync(value);
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, To) && !value.StartsWith("{"))
+                {
+                    to = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, To))
+                {
+                    to = await parseManager.ReplaceStlEntitiesForAttributeValueAsync(value);
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, Description) && !value.StartsWith("{"))
+                {
+                    description = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, Description))
+                {
+                    description = await parseManager.ReplaceStlEntitiesForAttributeValueAsync(value);
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, DeliveryArea) && !value.StartsWith("{"))
+                {
+                    deliveryArea = value;
+                }
+                else if (StringUtils.EqualsIgnoreCase(name, DeliveryArea))
+                {
+                    deliveryArea = await parseManager.ReplaceStlEntitiesForAttributeValueAsync(value);
+                }
+            }
+
+
+            var saveImgPath = Path.Combine(ImgData, "siteId" + parseManager.ContextInfo.Site.Id + "cId" + parseManager.ContextInfo.ContentId + "Id" +id +".jpg");
+
+            foreach (var item in SystemFonts.Families)
+            {
+                await Console.Out.WriteLineAsync(item.Name);
+            }
+
+            using (var image = new Image<Rgba32>(225, 200, Color.White))
+            {
+                var fontFamily = SystemFonts.Families.FirstOrDefault(f => f.Name == "Microsoft YaHei");
+                var boldFont = fontFamily.CreateFont(10, FontStyle.Bold);
+
+                image.Mutate(ctx => ctx.Fill(Color.Red));
+
+                // Draw the yellow rectangle at the top
+                var yellowRectTop = new Rectangle(0, 15, 225, 2);
+                image.Mutate(ctx => ctx.Fill(Color.Yellow, yellowRectTop));
+
+
+                // Draw the yellow rectangle inside the red one (bottom)
+                var yellowRectBottom = new Rectangle(0, 145, 225, 30);
+                image.Mutate(ctx => ctx.Fill(Color.Yellow, yellowRectBottom));
+
+
+
+                // Top yellow text
+                image.Mutate(ctx => ctx.DrawText(title, boldFont, Color.Yellow, new PointF(5, 2)));
+
+                image.Mutate(ctx => ctx.DrawText(logoUp, boldFont, Color.White, new PointF(85, 45)));
+
+                image.Mutate(ctx => ctx.DrawText(from, boldFont, Color.White, new PointF(15, 60)));
+                image.Mutate(ctx => ctx.DrawText("<—", boldFont, Color.White, new PointF(95, 57)));
+                image.Mutate(ctx => ctx.DrawText("—>", boldFont, Color.White, new PointF(95, 62)));
+                image.Mutate(ctx => ctx.DrawText(to, boldFont, Color.White, new PointF(135, 60)));
+                image.Mutate(ctx => ctx.DrawText(logoDown, boldFont, Color.White, new PointF(85, 80)));
+
+                image.Mutate(ctx => ctx.DrawText(description, boldFont, Color.Red, new PointF(15, 150)));
+
+                image.Mutate(ctx => ctx.DrawText(deliveryArea, boldFont, Color.White, new PointF(10, 186)));
+
+
+                // Save the image
+                image.Save(saveImgPath);
+            }
+            return saveImgPath.Split("wwwroot")[1];
+        }
+    }
+}
+
